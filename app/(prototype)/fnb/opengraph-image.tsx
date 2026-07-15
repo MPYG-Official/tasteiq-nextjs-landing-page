0ec0c25d1dadf81ae51ef8b3c08d848c545b03d6
runtime-theif <121952732+runtime-theif@users.noreply.github.com>
2026-07-15 21:05:03 +0530

    fix

--- app/(prototype)/fnb/opengraph-image.tsx
+++ app/(prototype)/fnb/opengraph-image.tsx
@@ -3,6 +3,6 @@ import { makeOgImageHandlers } from '@/lib/og-route-image';
 const { Image, alt } = makeOgImageHandlers('fnb');
 
 export { alt };
-export { contentType, revalidate, size } from '@/lib/create-og-image';
+export { contentType, size } from '@/lib/create-og-image';
 export const runtime = 'edge';
 export default Image;
